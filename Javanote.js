a57de53f7743b8cdef7e05f8b906b729d981dedf
Stephanie Moore <snmoore393@gmail.com>
2016-08-26 20:15:05 -0600

    adding dev server

---
+++ Javanote.js
@@ -0,0 +1,27 @@
+ //JavaScript Types
+
+ undefined //not present
+ null // intentionally undefined
+ undefined === null // false
+ NaN //not a number
+
+
+ true, false //boolean
+ 123 //number
+ 0x47e9 //number
+
+ "apple"// string
+ 'apple'// string
+
+ [1,2,3] //array (list)
+[1, "apple", false] // still an array
+
+function(){} // functions
+
+{
+	name: "value"
+} // objects
+
+"Wed Aug 24 2016 20:38:48 GMT-0600 (MDT)" // date
+
+\[a-z, 1-9]\/g // regex
