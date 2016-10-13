c19baea9aa707967c41ea843fb15a55be21a9969
Stephanie Moore <snmoore393@gmail.com>
2016-10-12 21:07:01 -0600

    backbone todo

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@ import navbar from './components/navbar';
 import todoController from './pages/todo';
 import d3Example from './pages/d3Example';
 import threeExample from './pages/threeExample';
+import bbTodoController from './pages/bb_todo';
 
 // on document load
 $(function(){
@@ -36,6 +37,9 @@ $(function(){
   case '/pages/todo.html': 
     todoController.init();
     break;
+  case '/pages/bb_todo.html': 
+    new bbTodoController();
+    break;
   case '/pages/d3Example.html': 
     d3Example.init();
     break;
@@ -46,8 +50,8 @@ $(function(){
 
  
   console.log('Hire me');
-  console.log('Please');
   console.log('Now');
+  console.log('Please');
 
   
 
