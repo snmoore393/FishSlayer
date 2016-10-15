2c46363c927a82d1fb1b0569e88190c2744fda11
Stephanie Moore <snmoore393@gmail.com>
2016-10-14 20:43:01 -0600

    todo backbone update

--- app/pages/bb_todo.js
+++ app/pages/bb_todo.js
@@ -20,9 +20,10 @@ var ControllerView = Backbone.View.extend({
     // alert('you have ' + this.model.get('todos').length +' todos!');
     var todos = this.model.get('todos');
     // render each todo item
+    var that = this;
     var renderedTodos = todos.map(function(item, index){
-      item.id = index;
-      var view = new bbTodoItemView(item);
+      item.id = index + 1;
+      var view = new bbTodoItemView(item, that);
       return view.$el;
     });
     // put all todo items into the dom
@@ -37,6 +38,24 @@ var ControllerView = Backbone.View.extend({
   addKeypress: function(event){
     if (event.which === 13) {
       this.addTodo();
+    }
+  },
+  removeTodo: function(id){
+    if (id >= 0){
+      this.model.removeTodo(id);
+      this.render();
+    }
+  },
+  editTodo: function(id, newTitle){
+    if (id >= 0){
+      this.model.editTodo(id, newTitle);
+      this.render();
+    }
+  },
+  changeComplete: function(id, newTitle){
+    if (id >= 0){
+      this.model.completeTodo(id, newTitle);
+      this.render();
     }
   }
 });
--- app/views/bb_todoItemView.js
+++ app/views/bb_todoItemView.js
@@ -1,17 +1,42 @@
+var $ = window.$;
 import Backbone from 'backbone';
 import _ from 'underscore';
 import itemTemplate from 'text!./todoItem.tpl';
 
 var todoItemView = Backbone.View.extend({
   tagName: 'div',
-  events: {},
-  initialize: function(item){
+  events: {
+    'click .close': 'removeHandler',
+    'click .edit': 'editHandler', 
+    'click .save': 'saveHandler',
+    'change input[type="checkbox"]': 'checkboxHandler' 
+  },
+  initialize: function(item, controller){
     // compile the template
+    this.id = item.id;
+    this.controller = controller;
     this.template = _.template(itemTemplate);
     this.render(item);
   },
   render: function(item){
     this.$el.html(this.template(item));
+  },
+  removeHandler: function(){
+    this.controller.removeTodo(this.id);
+  },
+  editHandler: function(){
+    // go into edit mode
+    this.$el.find('.edit').addClass('hidden');
+    this.$el.find('.save').removeClass('hidden');
+    this.$el.find('.todo-title').addClass('hidden');
+    this.$el.find('.todo-title-edit').removeClass('hidden');
+  },
+  saveHandler: function(){
+    var newTitle = $('.todo-title-edit input').val();
+    this.controller.editTodo(this.id, newTitle);
+  },
+  checkboxHandler: function(){
+    this.controller.changeComplete(this.id);
   }
 });
 
